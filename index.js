9049658c3ba47a8abc2da2d87243aa1c4a9b59c9
eternal <eternal.git@gmail.com>
2022-02-21 08:19:09 +0000

    fix

--- index.js
+++ index.js
@@ -2,26 +2,11 @@ const { Plugin } = require('powercord/entities');
 const { getModule } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
 
-const NativeTextAreaContextMenu = getModule(m => m?.default?.displayName === 'NativeTextAreaContextMenu', false);
-const SlateTextAreaContextMenu = getModule(m => m?.default?.displayName === 'SlateTextAreaContextMenu', false);
-const { expressionSuggestionsEnabled } = getModule(['expressionSuggestionsEnabled'], false);
-const Expressions = getModule(['toggleExpressionSuggestionsEnabled'], false);
+const Expressions = getModule(['useStickerSuggestionResults'], false);
 const Stickers = getModule(['queryStickers'], false);
 
 module.exports = class HideDMButtons extends Plugin {
    startPlugin() {
-      inject('dss-slate-text-area', SlateTextAreaContextMenu, 'default', (args, res) => {
-         res.props.children.splice(0, 1);
-
-         return res;
-      });
-
-      inject('dss-native-text-area', NativeTextAreaContextMenu, 'default', (args, res) => {
-         res.props.children.splice(0, 1);
-
-         return res;
-      });
-
       inject('dss-query-stickers', Stickers, 'queryStickers', ([, , channel], res) => {
          if (channel) {
             return { stickers: [] };
@@ -30,16 +15,13 @@ module.exports = class HideDMButtons extends Plugin {
          return res;
       });
 
-      Expressions._toggleExpressionSuggestionsEnabled = Expressions.toggleExpressionSuggestionsEnabled;
-      Expressions.toggleExpressionSuggestionsEnabled = () => void 0;
-
-      if (expressionSuggestionsEnabled) Expressions._toggleExpressionSuggestionsEnabled();
+      inject('dss-sticker-results', Expressions, 'useStickerSuggestionResults', (args, res) => {
+         return [];
+      });
    }
 
    pluginWillUnload() {
-      Expressions.toggleExpressionSuggestionsEnabled = Expressions._toggleExpressionSuggestionsEnabled;
-      uninject('dss-slate-text-area');
-      uninject('dss-native-text-area');
+      uninject('dss-sticker-results');
       uninject('dss-query-stickers');
    }
 };
